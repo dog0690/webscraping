c213662d53527247bf75899de6f89c68bf7971c9
dog0690 <charleymaivn@gmail.com>
2024-11-21 11:35:48 -0500

    webscraping practice

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from bs4 import BeautifulSoup
+import requests
+
+url = ('https://finance.yahoo.com/markets/crypto/active/')
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+table = soup.find("tbody", class_ = "body yf-paf8n5")
